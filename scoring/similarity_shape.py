8003e2ca5852f4474f57eaa71f282a26731f17b9
iwatobipen <seritaka@gmail.com>
2021-01-25 11:01:48 +0900

    update

--- scoring/similarity_shape.py
+++ scoring/similarity_shape.py
@@ -23,11 +23,13 @@ except:
 class shape_tanimoto(object):
     """Scores based on the Tanimoto similarity to a query SMILES. Supports a similarity cutoff k."""
 
-    def __init__(self, query_molfile: str, k=1.0, maxIter=10):
+    def __init__(self, query_molfile: str, k=1.0, maxIter=10, opt=False,):
         self.k = k
         self.maxIter = maxIter
         self.query_mol = Chem.MolFromMolFile(query_molfile)
-        AllChem.MMFFOptimizeMolecule(self.query_mol)
+        self.opt = opt
+        if self.opt:
+            AllChem.MMFFOptimizeMolecule(self.query_mol, maxIters=500)
 
     def __call__(self, smiles: List[str]) -> dict:
         mols = [Chem.MolFromSmiles(smile) for smile in smiles]
@@ -36,15 +38,16 @@ class shape_tanimoto(object):
         for idx, mol in enumerate(mols):
             if valid[idx] == 1:
                 #mol = Chem.AddHs(mol)
-                res = AllChem.EmbedMolecule(mol)
-                if res==0:
-                    try:
-                        res=AllChem.MMFFOptimizeMolecule(mol, confId=0)
-                        if res==0:
-                            tmp_valid.append(1)
-                        else:
-                            tmp_valid.append(0)
-                    except: pass
+                try:
+                    confres = AllChem.EmbedMolecule(mol,)
+                except:
+                    confres = None
+                if confres == 0:
+                    res = AllChem.MMFFOptimizeMolecule(mol, maxIters=1000, confId=0)
+                    if res>=0:
+                        tmp_valid.append(1)
+                    else:
+                        tmp_valid.append(0)
                 else:
                     tmp_valid.append(0)
             else:
@@ -54,11 +57,14 @@ class shape_tanimoto(object):
         valid_mols = [mols[idx] for idx in valid_idxs]
         for idx, mol in enumerate(valid_mols):
             mol.SetProp("_Name",str(idx))
-            AllChem.EmbedMolecule(mol)
         #fps = [AllChem.GetMorganFingerprint(mol) for mol in valid_mols]
-        shape_score = np.array([cpyshapeit.AlignMol(self.query_mol, m, maxIter=self.maxIter, whichScore="Shape-it::Tversky_ref") for m in valid_mols])
+        shape_score = []
+        for m in valid_mols:
+            #score = cpyshapeit.AlignMol(self.query_mol, m, maxIter=self.maxIter, whichScore="Shape-it::Tversky_Ref")
+            score = cpyshapeit.AlignMol(self.query_mol, m, maxIter=self.maxIter)
+            shape_score.append(score)
+        shape_score = np.array(shape_score)
         tanimoto = np.minimum(shape_score, self.k) / self.k
-        # tversky_similarity = np.square(tanimoto)
         score = np.full(len(smiles), 0, dtype=np.float32)
 
         for idx, value in zip(valid_idxs, tanimoto):
